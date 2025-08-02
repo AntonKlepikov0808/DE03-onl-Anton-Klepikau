5f0f1774c3ffcc67eac7eeda1643dc2a57d54bff
ANTONPC\klepi <klepikovvv4@gmail.com>
2025-08-02 22:31:52 +0300

    ДЗ1

---
+++ Python/Ex1/#задание 1.py
@@ -0,0 +1,13 @@
+#задание 1
+a = int(input('Введите число : '))
+b = int(input('Введите число : '))
+c = a+b
+print (c)
+
+
+
+#задание 2
+print ('I love (hate?) Python')
+#или 
+a = input('Введите текст: ')
+print(a)
